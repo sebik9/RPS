31d5f26d8a2dbefcb64033e2b6ab16893553c2db
sebik9 <s3bik9@seznam.cz>
2023-11-14 16:57:00 +0100

    Update
    RPS update complete

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,72 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>Rock Paper Scissors</h2>
+  <input v-model="playerInput" placeholder="Your Choice...">
+  <button @click="getPlayerInput">Play</button>
+  <div>Player Choice: {{ playerChoiceDisp }}</div>
+  <div>Computer Choice: {{ computerChoiceDisp }}</div>
+  <div>Result: {{ result }}</div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      playerInput: '',
+      playerChoiceDisp: '', 
+      computerChoiceDisp: '', 
+      result: ''
+    }
+  },
+  methods: {
+    getPlayerInput() {
+      const input = this.playerInput.toLowerCase();
+      if (input === "rock" || input === "paper" || input === "scissors") {
+        this.displayGameResult(input);
+      } else {
+        alert("choice must be rock, paper, or scissors");
+      }
+    },
+    getComputerChoice() {
+      const randomNum = Math.floor(Math.random() * 3);
+      switch (randomNum) {
+        case 0:
+          return "rock";
+        case 1:
+          return "paper";
+        case 2:
+          return "scissors";
+      }
+    },
+    displayGameResult(userChoice) {
+      const computerChoice = this.getComputerChoice();
+      this.playerChoiceDisp = userChoice; 
+      this.computerChoiceDisp = computerChoice; 
+      const result = this.determineWinner(userChoice, computerChoice);
+      this.result = result;
+    },
+    determineWinner(userChoice, computerChoice) {
+      if (userChoice === computerChoice) {
+        return "Tie";
+      }
+      if (userChoice === "rock") {
+        return computerChoice === "paper" ? "PC won" : "Player won";
+      }
+      if (userChoice === "paper") {
+        return computerChoice === "scissors" ? "PC won" : "Player won";
+      }
+      if (userChoice === "scissors") {
+        return computerChoice === "rock" ? "PC won" : "Player won";
+      }
+      return "Invalid choice";
+    }
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
